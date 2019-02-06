c231f16e006003b58fb8da7a6f3123e38722c251
Kartik Chauhan <kckartik17@gmail.com>
2019-02-06 21:37:16 +0530

    added converter route
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:unit_convertor_app/category.dart';
+import 'package:unit_convertor_app/unit.dart';
 
 final _backgroundColor = Colors.green[100];
 
@@ -36,6 +37,16 @@ class CategoryRoute extends StatelessWidget {
     );
   }
 
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final categories = <Category>[];
@@ -45,6 +56,7 @@ class CategoryRoute extends StatelessWidget {
         name: _categoryNames[i],
         color: _baseColors[i],
         iconLocation: Icons.cake,
+        units: _retrieveUnitList(_categoryNames[i]),
       ));
     }
 
