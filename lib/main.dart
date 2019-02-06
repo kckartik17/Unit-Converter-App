24a41b6055ae069b20e0f1f549bed7db17ad639c
Kartik Chauhan <kckartik17@gmail.com>
2019-02-06 20:55:21 +0530

    02: Category Widget
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:unit_convertor_app/category.dart';
+
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+void main() => runApp(UnitConverterApp());
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          child: Category(
+            name: _categoryName,
+            color: _categoryColor,
+            iconLocation: _categoryIcon,
+          ),
+        ),
+      ),
+    );
+  }
+}
