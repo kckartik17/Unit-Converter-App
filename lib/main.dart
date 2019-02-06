2a2e0084259387161d3e2cc3ae58b71cd1a8f4e1
Kartik Chauhan <kckartik17@gmail.com>
2019-02-07 02:11:21 +0530

    Updated code
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:unit_convertor_app/category_route.dart';
 
-const _categoryName = 'Cake';
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
-
 void main() => runApp(UnitConverterApp());
 
 class UnitConverterApp extends StatelessWidget {
